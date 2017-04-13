5387b6e493e3b8599a07effc64b2455cb780e11c
sumeyyertas <sumeyye.ertas@gmail.com>
2017-04-13 16:03:45 +0300

    V.1.0

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Singleton
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
@@ -25,51 +25,6 @@ namespace Singleton
             Console.WriteLine("Division : " + Calculate.Instance.Division());
 
             Console.ReadLine();
-        }
-    }
-
-    public sealed class Calculate
-    {
-        private Calculate()
-        {
-
-        }
-
-        private static Calculate instance = null;
-        public static Calculate Instance
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    instance = new Calculate();
-                }
-                return instance;
-            }
-        }
-
-
-        public double ValueOne { get; set; }
-        public double ValueTwo { get; set; }
-
-        public double Addition()
-        {
-            return ValueOne + ValueTwo;
-        }
-
-        public double Subtraction()
-        {
-            return ValueOne - ValueTwo;
-        }
-
-        public double Multiplication()
-        {
-            return ValueOne * ValueTwo;
-        }
-
-        public double Division()
-        {
-            return ValueOne / ValueTwo;
         }
     }
 }
